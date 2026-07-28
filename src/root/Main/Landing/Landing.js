b1ea2df78e9860157531168f8130a369b97fdf15
Roxanne Tabuena <roxyannet@gmail.com>
2026-07-28 14:14:13 -0700

    add basic info

--- src/root/Main/Landing/Landing.js
+++ src/root/Main/Landing/Landing.js
@@ -3,7 +3,9 @@ import { Stream } from '../Stream/Stream'
 export const Landing = () => {
     return (
         <div className={style.landing}>
-            landing
+            <img src="https://imagehostclub.s3.us-west-1.amazonaws.com/dreamers/logo_2.png" alt="dreamers Only Logo"></img>
+            <h1>Dreamers Only</h1>
+            <h2>the work and musings of Roxanne Tabuena</h2>
             <Stream/>
         </div>
     )
