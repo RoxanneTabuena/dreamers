9533126a8def4573981b627bb2e92737833cbb6b
Roxanne Tabuena <roxyannet@gmail.com>
2026-07-29 09:55:51 -0700

    add nav

--- src/root/Head/Nav/Nav.js
+++ src/root/Head/Nav/Nav.js
@@ -1,8 +1,11 @@
 import style from './nav.module.css'
+import { Tag } from '../../Main/Tag/Tag'
+import { tags } from '../../Main/Stream/content'
 export const Nav = () => {
+    const menu = Object.keys(tags).map((t)=>{return <Tag tag={t} key={t}/>})
     return (
         <div className={style.nav}>
-
+            {menu}
         </div>
     )
 }
